d498a735f9757c2dd46888922eece0060d491fd2
NexDoorSg <admin@nexdoor.sg>
2026-04-16 18:07:02 +0800

    Update page.tsx

--- src/app/hdb-valuation-jurong-west/page.tsx
+++ src/app/hdb-valuation-jurong-west/page.tsx
@@ -142,9 +142,10 @@ export default function JurongWestValuationPage() {
             </p>
 
             <ul className="mt-2 space-y-1 text-sm">
+              
               <li>
                 <a href="/hdb-valuation-tampines" className="underline text-[#8b6b52] hover:text-[#6f5440]">
-                  HDB Valuation Jurong West
+                  HDB Valuation Tampines
                 </a>
               </li>
               <li>
@@ -158,8 +159,28 @@ export default function JurongWestValuationPage() {
                 </a>
               </li>
               <li>
-                <a href="/hdb-valuation-jurong-west" className="underline text-[#8b6b52] hover:text-[#6f5440]">
-                  HDB Valuation Jurong West
+                <a href="/hdb-valuation-bedok" className="underline text-[#8b6b52] hover:text-[#6f5440]">
+                  HDB Valuation Bedok
+                </a>
+              </li>
+              <li>
+                <a href="/hdb-valuation-sengkang" className="underline text-[#8b6b52] hover:text-[#6f5440]">
+                  HDB Valuation Sengkang
+                </a>
+              </li>
+              <li>
+                <a href="/hdb-valuation-punggol" className="underline text-[#8b6b52] hover:text-[#6f5440]">
+                  HDB Valuation Punggol
+                </a>
+              </li>
+              <li>
+                <a href="/hdb-valuation-ang-mo-kio" className="underline text-[#8b6b52] hover:text-[#6f5440]">
+                  HDB Valuation Ang Mo Kio
+                </a>
+              </li>
+              <li>
+                <a href="/hdb-valuation-bishan" className="underline text-[#8b6b52] hover:text-[#6f5440]">
+                  HDB Valuation Bishan
                 </a>
               </li>
             </ul>
