6137dbb81f0a376e1efd7314ba2ce6caea3637b9
NexDoorSg <admin@nexdoor.sg>
2026-04-22 23:07:48 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
           <img
             height="1"
             width="1"
-            style={{ display: "none" }}
+            style={{ display: 'none' }}
             src="https://www.facebook.com/tr?id=947297821608514&ev=PageView&noscript=1"
             alt=""
           />
