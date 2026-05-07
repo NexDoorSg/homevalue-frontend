0dbf38eb56c1eac8f2a33d83403cfd6a0b080047
NexDoorSg <admin@nexdoor.sg>
2026-05-07 16:16:15 +0800

    Load HomeValue area unit selector

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import HomeValueLeadPopupExperiment from "@/components/HomeValueLeadPopupExperiment";
+import HomeValueAreaUnitSelector from "@/components/HomeValueAreaUnitSelector";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -62,6 +63,7 @@ export default function RootLayout({
           `}
         </Script>
         <HomeValueLeadPopupExperiment />
+        <HomeValueAreaUnitSelector />
         <noscript>
           <img
             height="1"
