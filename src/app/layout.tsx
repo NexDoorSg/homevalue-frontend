43c134171ca670f4f30d854ff7b55770b2e3e68d
NexDoorSg <admin@nexdoor.sg>
2026-05-20 14:52:52 +0800

    Load HomeValue visual audit overrides

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Montserrat, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import "./homevalue-visual-fixes.css";
 import Script from "next/script";
 import HomeValueLeadPopupExperiment from "@/components/HomeValueLeadPopupExperiment";
 import HomeValueAreaUnitSelector from "@/components/HomeValueAreaUnitSelector";
